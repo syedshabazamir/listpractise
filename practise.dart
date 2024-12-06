e3a6ff53b98f3549737342534f280b613909fe9e
syedshabazamir <syedshabazamir553@gmail.com>
2024-12-06 22:31:02 +0500

    first commit

---
+++ practise.dart
@@ -0,0 +1,14 @@
+void main(){
+List<String> names = ["Ali","Shahrukh","Saleem","Amir"];
+var e1 = names.first;
+var e2 = names.last;
+var e3 = names.elementAt(1);
+var len = names.length;
+var secondLast = names.elementAt(3);
+print(names);
+print(e1);
+print(e2);
+print(e3);
+print(len);
+print(secondLast);
+}
